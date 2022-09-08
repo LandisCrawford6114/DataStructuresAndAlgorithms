9813c156e4906d4f4edc7c057c3919ae1b65b247
LandisCrawford6114 <Landis.Crawford6114@gmail.com>
2022-09-08 15:14:18 +0000

    it work

--- node.c
+++ node.c
@@ -24,6 +24,50 @@ void insert(int key, int data)
     head = new;
 }
 
+void insertEnd(int key, int data)
+{
+    //Reserve memory
+    struct node *new = (struct node*)malloc(sizeof(struct node));
+    
+    //Fill data
+    new->key = key;
+    new->data = data;
+
+    //Connect node
+    current = head;
+    while(current->next != NULL)
+    {
+        current = current->next;
+    }
+    current->next = new;
+}
+
+void insertAt(int key, int data, int location)
+{
+    //Reserve memory
+    struct node *new = (struct node*)malloc(sizeof(struct node));
+    
+    //Fill data
+    new->key = key;
+    new->data = data;
+
+    //Connect node
+    current = head;
+    while(current->next != NULL && current->key != location)
+    {
+        current = current->next;
+    }
+    if(current->key == location)
+    {
+        new->next = current->next;
+        current->next = new;
+    }
+    else
+    {
+        printf("Insertion failed: key not found");
+    }
+}
+
 void print()
 {
     current = head;
@@ -54,6 +98,7 @@ void delete(int key)
     if(current->key==key)
     {
         head = head->next;
+        printf("Node Deleted\n");
     }
     else{
         while(next != NULL && next->key != key)
@@ -61,17 +106,44 @@ void delete(int key)
             current = next;
             next = next->next;
         }
-        current->next = next->next;
+        if(next->key == key)
+        {
+            current->next = next->next;
+            printf("Node Deleted\n");
+        }
+        else
+        {
+            printf("Delete failed: key not found\n");
+        }
     }
 
+}
+
+void search(int key)
+{
+    current = head;
+    while(current->next != NULL && current->key != key)
+    {
+        current = current->next;
+    }
+    if(current->key == key)
+    {
+        printf("Node found: data=%d\n", current->data);
+    }
+    else
+    {
+        printf("Search failed: key not found\n");
+    }
 }
 
 int main()
 {
     insert(1, 6);
     insert(2, 7);
-    insert(3, 8);
-    delete(3);
+    insertEnd(3, 8);
+    insertAt(4, 9, 1);
+    delete(2);
+    search(4);
     length();
     print();
     return 0;
