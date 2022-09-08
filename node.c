8d13d0930786806ea3a41237bd3a30cc61709a69
LandisCrawford6114 <Landis.Crawford6114@gmail.com>
2022-09-08 16:04:20 +0000

    UI

--- node.c
+++ node.c
@@ -76,6 +76,7 @@ void print()
         printf("%d \n",current->data);
         current = current->next;
     }
+    printf("\n");
 }
 
 void length()
@@ -87,7 +88,7 @@ void length()
         length++;
         current = current->next;
     }
-    printf("Length: %d \n",length);
+    printf("Length: %d \n\n",length);
 }
 
 void delete(int key)
@@ -100,7 +101,7 @@ void delete(int key)
         struct node *old = head;
         head = head->next;
         free(old);
-        printf("Node Deleted\n");
+        printf("Node Deleted\n\n");
     }
     else{
         while(next != NULL && next->key != key)
@@ -113,11 +114,11 @@ void delete(int key)
             struct node *old = next;
             current->next = next->next;
             free(old);
-            printf("Node Deleted\n");
+            printf("Node Deleted\n\n");
         }
         else
         {
-            printf("Delete failed: key not found\n");
+            printf("Delete failed: key not found\n\n");
         }
     }
 
@@ -132,23 +133,93 @@ void search(int key)
     }
     if(current->key == key)
     {
-        printf("Node found: data=%d\n", current->data);
+        printf("Node found: data=%d\n\n", current->data);
     }
     else
     {
-        printf("Search failed: key not found\n");
+        printf("Search failed: key not found\n\n");
     }
 }
 
 int main()
 {
-    insert(1, 6);
-    insert(2, 7);
-    insertEnd(3, 8);
-    insertAt(4, 9, 1);
-    delete(2);
-    search(4);
-    length();
-    print();
+    int input, key, data, location, insertType, printType;
+    // insert(1, 6);
+    // insert(2, 7);
+    // insertEnd(3, 8);
+    // insertAt(4, 9, 1);
+    // delete(2);
+    // search(4);
+    // length();
+    // print();
+    while(input != 5)
+    {
+        printf("1-Insert\n");
+        printf("2-Delete\n");
+        printf("3-Search\n");
+        printf("4-Print\n");
+        printf("5-You're done\n");
+        printf("Your Choice: ");
+        scanf("%d", &input);
+        printf("\n");
+        if(input == 1)
+        {
+            printf("1-Insert at Start\n");
+            printf("2-Insert at End\n");
+            printf("3-Insert at Key\n");
+            printf("Your Choice: ");
+            scanf("%d", &insertType);
+            printf("\n");
+            printf("Key: ");
+            scanf("%d", &key);
+            printf("Data: ");
+            scanf("%d", &data);
+            if(insertType == 1)
+            {
+                insert(key, data);
+            }
+            else if(insertType == 2)
+            {
+                insertEnd(key, data);
+            }
+            else if(insertType == 3)
+            {
+                printf("\nLocation: ");
+                scanf("%d", &location);
+                insertAt(key, data, location);
+            }
+            printf("\n");
+        }
+        else if(input == 2)
+        {
+            printf("Key: ");
+            scanf("%d", &key);
+            printf("\n");
+            delete(key);
+        }
+        else if(input == 3)
+        {
+            printf("Key: ");
+            scanf("%d", &key);
+            printf("\n");
+            search(key);
+        }
+        else if(input == 4)
+        {
+            printf("1-Print List\n");
+            printf("2-Print Length\n");
+            printf("Your Choice: ");
+            scanf("%d", &printType);
+            printf("\n");
+            if(printType == 1)
+            {
+                print();
+            }
+            else if(printType == 2)
+            {
+                length();
+            }
+        }
+    }
     return 0;
 }
